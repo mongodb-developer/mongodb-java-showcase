c14821f49ad142ff2466e350ccc65e84d807fbc1
Ricardo Mello <ricardohsmello@gmail.com>
2025-06-18 02:00:21 -0300

    Refactoring MovieController and included new title-year endpoint

--- src/main/java/com/mongodb/MovieController.java
+++ src/main/java/com/mongodb/MovieController.java
@@ -1,10 +1,13 @@
 package com.mongodb;
 
+import org.bson.Document;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
@@ -16,12 +19,20 @@ public class MovieController {
 		this.movieService = movieService;
 	}
 
-	@GetMapping("/simulate-load")
-	public ResponseEntity<Void> search(
-			@RequestParam String plot1,
-			@RequestParam String plot2
+	@GetMapping("/enriched-details")
+	public ResponseEntity<List<Document>> search(
+			@RequestParam String plot
 	) {
-		movieService.simulateConcurrentSearch(plot1, plot2);
-		return ResponseEntity.accepted().build();
+		return ResponseEntity.ok(movieService.getMovies(plot));
 	}
+
+
+	@GetMapping("/by-title-year")
+	public ResponseEntity<List<Document>> findByTitleAndYear(
+			@RequestParam String title,
+			@RequestParam int year
+	) {
+		return ResponseEntity.ok(movieService.findByTitleAndYear(title, year));
+	}
+
 }
